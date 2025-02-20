c1686e0521ade118aee01857fe5c962bd1f2b4f4
skeldonky <75779367+skeldonky@users.noreply.github.com>
2025-02-20 20:25:07 +0900

    GameMode commit
    스포너 랜덤 좌표 생성 제작
    맵 꾸미기 및 몇몇 에셋 추가

--- Shadow_of_the_Desert/Source/Shadow_of_the_Desert/EnemySpawner.cpp
+++ Shadow_of_the_Desert/Source/Shadow_of_the_Desert/EnemySpawner.cpp
@@ -2,7 +2,9 @@
 
 
 #include "EnemySpawner.h"
+#include "DrawDebugHelpers.h"
 #include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AEnemySpawner::AEnemySpawner()
@@ -28,6 +30,38 @@ FVector AEnemySpawner::GetRandomPointInVolume() const
 		FMath::FRandRange(-BoxExtent.Y, BoxExtent.Y),
 		FMath::FRandRange(-BoxExtent.Z, BoxExtent.Z)
 	);
+}
+
+void AEnemySpawner::EnemySpawn()
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("Player"), FoundActors);
+
+	const float AvoidanceRadius = 300.0f;
+	bool bValidLocation = false;
+	FVector SpawnLocation;
+
+	while (!bValidLocation)
+	{
+		SpawnLocation = GetRandomPointInVolume();
+
+		bValidLocation = true; // 유효한 위치라고 가정
+
+		for (AActor* Actor : FoundActors)
+		{
+			if (Actor)
+			{
+				float Distance = FVector::Dist(SpawnLocation, Actor->GetActorLocation());
+				if (Distance < AvoidanceRadius)
+				{
+					bValidLocation = false;
+					// 여기 아래에 몬스터 스폰 코드 추가 or 몬스터 생성 함수
+
+					break; // 가까운 플레이어가 있으면 다시 좌표 생성
+				}
+			}
+		}
+	}
 }
 
 // Called when the game starts or when spawned
