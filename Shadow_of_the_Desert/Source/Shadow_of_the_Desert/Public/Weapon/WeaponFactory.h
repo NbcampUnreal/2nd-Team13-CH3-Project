f1ee5455e45ed159745bac98ceec5733733fd9c0
jch0422 <jeongchanhyotr@gmail.com>
2025-02-20 20:24:55 +0900

    Weapon
    무기, 총알 스탯, 재장전, 총알 발사

---
+++ Shadow_of_the_Desert/Source/Shadow_of_the_Desert/Public/Weapon/WeaponFactory.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "WeaponFactory.generated.h"
+
+
+UCLASS()
+class SHADOW_OF_THE_DESERT_API UWeaponFactory : public UObject
+{
+	GENERATED_BODY()
+	
+};
