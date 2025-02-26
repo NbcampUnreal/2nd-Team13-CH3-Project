9b58759f0ce09c185b33381cddb8e22e04a10cdf
asdfg7936 <swa0708@gmail.com>
2025-02-26 10:02:34 +0900

    1차 수정

---
+++ Shadow_of_the_Desert/Source/Shadow_of_the_Desert/Public/Bullet/ExplosiveBullet.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bullet/BulletBase.h"
+#include "ExplosiveBullet.generated.h"
+
+
+UCLASS()
+class SHADOW_OF_THE_DESERT_API AExplosiveBullet : public ABulletBase
+{
+	GENERATED_BODY()
+public:
+	AExplosiveBullet();
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float ExplosionRadius; // Æø¹ß ¹üÀ§
+
+	void BeginPlay() override;
+	UFUNCTION()
+	void Explode(FVector Location);
+
+	
+	virtual void OnHit(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	) override;
+};
--- Shadow_of_the_Desert/Source/Shadow_of_the_Desert/Shadow_of_the_DesertCharacter.cpp
+++ Shadow_of_the_Desert/Source/Shadow_of_the_Desert/Shadow_of_the_DesertCharacter.cpp
@@ -35,7 +35,7 @@ AShadow_of_the_DesertCharacter::AShadow_of_the_DesertCharacter()
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->JumpZVelocity = 550.f;
 	GetCharacterMovement()->AirControl = 0.35f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
